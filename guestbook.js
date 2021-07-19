dc30378c2a21ca8a2049802eea1a2f8561df367e
ExoticRoots <contact@fredesk.com>
2021-07-19 11:06:25 -0500

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://07cb76ea.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
